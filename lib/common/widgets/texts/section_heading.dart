0099e8004edaaa9712a058c2781683512fbbb035
Alghany Kennedy <alghanyka@gmail.com>
2023-10-30 09:29:35 +0700

    feat: Searchbar & Horizontal ListView builder Categories

---
+++ lib/common/widgets/texts/section_heading.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+class TSectionHeading extends StatelessWidget {
+  const TSectionHeading({
+    super.key,
+    this.textColor,
+    this.shwowActionButton = true,
+    required this.title,
+    this.buttonTitle = 'View All',
+    this.onPressed,
+  });
+
+  final Color? textColor;
+  final bool shwowActionButton;
+  final String title, buttonTitle;
+  final void Function()? onPressed;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Text(
+          title,
+          style: Theme.of(context)
+              .textTheme
+              .headlineSmall!
+              .apply(color: textColor),
+          maxLines: 1,
+          overflow: TextOverflow.ellipsis,
+        ),
+        if (shwowActionButton)
+          TextButton(
+            onPressed: onPressed,
+            child: Text(
+              buttonTitle,
+            ),
+          ),
+      ],
+    );
+  }
+}
--- lib/features/shop/screens/home/home.dart
+++ lib/features/shop/screens/home/home.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:techtrolley/common/widgets/custom_shapes/container/primary_header_container.dart';
+import 'package:techtrolley/common/widgets/custom_shapes/container/search_container.dart';
+import 'package:techtrolley/common/widgets/texts/section_heading.dart';
 import 'package:techtrolley/features/shop/screens/home/widgets/home_appbar.dart';
+import 'package:techtrolley/features/shop/screens/home/widgets/home_categories.dart';
+import 'package:techtrolley/utils/constants/sizes.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -12,8 +16,44 @@ class HomeScreen extends StatelessWidget {
         child: Column(
           children: [
             TPrimaryHeaderContainer(
-              child: THomeAppBar(),
-            ),
+                child: Column(
+              children: [
+                /// -- Appbar --
+                THomeAppBar(),
+                SizedBox(
+                  height: TSizes.spaceBtwItems,
+                ),
+
+                /// -- Searchbar --
+                TSearchContainer(
+                  text: 'Search in Store',
+                ),
+                SizedBox(
+                  height: TSizes.spaceBtwSections,
+                ),
+
+                /// -- Categories --
+                Padding(
+                  padding: EdgeInsets.only(left: TSizes.defaultSpace),
+                  child: Column(
+                    children: [
+                      /// -- Heading
+                      TSectionHeading(
+                        title: 'Popular Categories',
+                        shwowActionButton: false,
+                        textColor: Colors.white,
+                      ),
+                      SizedBox(
+                        height: TSizes.spaceBtwSections,
+                      ),
+
+                      /// Categories
+                      THomeCategories()
+                    ],
+                  ),
+                ),
+              ],
+            )),
           ],
         ),
       ),
