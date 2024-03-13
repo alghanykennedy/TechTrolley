1e5637076f2f4956e71870596341acb9704c3264
Alghany Kennedy Adam <alghany.adam@royalcorp.id>
2024-03-13 15:57:49 +0700

    feat: All Products

--- lib/features/shop/screens/home/home.dart
+++ lib/features/shop/screens/home/home.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:techtrolley/common/widgets/custom_shapes/container/primary_header_container.dart';
 import 'package:techtrolley/common/widgets/custom_shapes/container/search_container.dart';
 import 'package:techtrolley/common/widgets/layouts/grid_layout.dart';
 import 'package:techtrolley/common/widgets/products/product_cards/product_card_vertical.dart';
 import 'package:techtrolley/common/widgets/texts/section_heading.dart';
+import 'package:techtrolley/features/shop/screens/all_products/all_products.dart';
 import 'package:techtrolley/features/shop/screens/home/widgets/home_appbar.dart';
 import 'package:techtrolley/features/shop/screens/home/widgets/home_categories.dart';
 import 'package:techtrolley/features/shop/screens/home/widgets/promo_slider.dart';
@@ -82,7 +84,7 @@ class HomeScreen extends StatelessWidget {
                   /// -- Heading
                   TSectionHeading(
                     title: 'Popular Products',
-                    onPressed: () {},
+                    onPressed: () => Get.to(() => const AllProducts()),
                   ),
 
                   /// -- Popular Products
